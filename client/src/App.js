72a140da886e9537cf78149ecf495f5762506ef7
Turhan Sel <turhansel@gmail.com>
2022-02-14 01:25:34 +0300

    :sparkles: feat: add router and navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignIn from './pages/Auth/SignIn';
+import ProductDetail from './pages/ProductDetail';
 import SignUp from './pages/Auth/SignUp';
 import Products from './pages/Products';
 
 function App() {
-	
-	return (
-		<BrowserRouter>
-			<div>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Products />} />
-					<Route path='/signin' element={<SignIn />} />
-					<Route path='/signup' element={<SignUp />} />
-				</Routes>
-			</div>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Products />} />
+          <Route path='product/:product_id' element={<ProductDetail />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,28 +3,30 @@ import { Link } from 'react-router-dom';
 import { Button } from '@chakra-ui/react';
 
 const Navbar = () => {
-	return (
-		<nav className='flex items-center justify-between border-b border-b-slate-200  p-3'>
-			<div className='flex'>
-				<div className=''>
-					<Link to='/'>eCommerce</Link>
-				</div>
-				<ul className='ml-10 flex'>
-					<li>
-						<Link to='/'>Products</Link>
-					</li>
-				</ul>
-			</div>
-			<div className='space-x-2'>
-				<Link to='/signin'>
-					<Button colorScheme='pink'>Login</Button>
-				</Link>
-				<Link to='/signup'>
-					<Button colorScheme='pink'>Register</Button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className='flex items-center justify-between rounded-3xl bg-gradient-to-t from-indigo-400 to-[#32f9b0] p-4 mix-blend-hard-light shadow-lg backdrop-blur-2xl '>
+      <div className='flex'>
+        <div className=''>
+          <Link to='/' className='font-bold'>
+            e-Konserve
+          </Link>
+        </div>
+        <ul className='ml-10 flex'>
+          <li>
+            <Link to='/'>Products</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='space-x-2'>
+        <Link to='/signin'>
+          <Button colorScheme='orange'>Login</Button>
+        </Link>
+        <Link to='/signup'>
+          <Button colorScheme='orange'>Register</Button>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
