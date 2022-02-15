2eb1d0c15ec0041102473f5058915ee7cbed7d14
Turhan Sel <turhansel@gmail.com>
2022-02-16 00:49:38 +0300

    :sparkles: feat: create register and logout requests

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
+axios.interceptors.request.use(
+  function (config) {
+    const { origin } = new URL(config.url);
+    const allowedOrigins = [process.env.REACT_APP_BASE_ENDPOINT];
+    const token = localStorage.getItem('access-token');
 
+    if (allowedOrigins.includes(origin)) {
+      config.headers.authorization = token;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
 
 export const fetchProductList = async ({ pageParam = 1 }) => {
   const { data } = await axios.get(
@@ -12,6 +26,30 @@ export const fetchProductList = async ({ pageParam = 1 }) => {
 export const fetchProduct = async (product_id) => {
   const { data } = await axios.get(
     `${process.env.REACT_APP_BASE_ENDPOINT}/product/${product_id}`
+  );
+  return data;
+};
+
+export const fetchRegister = async (input) => {
+  const { data } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/register`,
+    input
+  );
+  return data;
+};
+
+export const fetchIsAuth = async (input) => {
+  const { data } = await axios.get(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/me`,
+    input
+  );
+  return data;
+};
+
+export const fetchLogout = async () => {
+  const { data } = await axios.post(
+    `${process.env.REACT_APP_BASE_ENDPOINT}/auth/logout`,
+    { refresh_token: localStorage.getItem('refresh-token') }
   );
   return data;
 };
